04832e72d9b001ab7894a01f0e49ce277e9b5e99
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-10 11:24:13 +0300

    prac

--- js/practice.js
+++ js/practice.js
@@ -10,13 +10,18 @@ const paraDivEl = document.getElementById('paraDiv');
 console.log(btn2El, paraDivEl);
 
 // klausomes paspaudimo ant mygtuko
-btn2El.addEventListener('click', () => {
-  console.log('pasp');
-  addPEl();
-});
+// btn2El.addEventListener('click', () => {
+//   console.log('pasp');
+//   addPEl();
+// });
+btn2El.addEventListener('click', addPEl);
 
 function addPEl() {
   // suukurti nauja p el
+  const pEl = document.createElement('p');
   // prideti i ji text
+  pEl.textContent =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, itaque.';
   // patalpinti ji paraDivEl (apend)
+  paraDivEl.append(pEl);
 }
